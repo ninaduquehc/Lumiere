d2a8901c0f36145221ff1d418fba28554eebd3d4
Marina Duque de Holanda Cavalcanti <marinaduque2006@gmail.com>
2026-05-21 15:24:49 +0000

    add.2

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
-@app.route("/contato")
-def contato():
-    return render_template("contato.html")
+@app.route("/catalogo")
+def catalogo():
+    return render_template("catalogo.html")
 
-@app.route("/sobre")
-def sobre():
-    return render_template("sobre.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
